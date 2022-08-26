cf56331a8e31fafa69f8daa5864fe82ccc1d0a2b
Saransh Sharma <saransh03sharma@gmail.com>
2022-08-26 16:12:28 +0530

    a3 submission

--- Assignments/Assign 3/Ripple_Carry_Adder/Ripple_Carry_Adder_64bit.v
+++ Assignments/Assign 3/Ripple_Carry_Adder/Ripple_Carry_Adder_64bit.v
@@ -17,6 +17,11 @@
 // Revision 0.01 - File Created
 // Additional Comments: 
 //
+// Assignement No.: 3
+// Semester: Autumn 2022
+// Group No.: 65
+// Member 1: Pranav Mehrotra (20CS10085)
+// Member 2: Saransh Sharma (20CS30065)
 //////////////////////////////////////////////////////////////////////////////////
 module Ripple_Carry_Adder_64bit(input[63:0] in1,input[63:0] in2, input cin, output [63:0] sum, output cout);
 wire carry;
