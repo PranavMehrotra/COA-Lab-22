f3a1e93fb48b2479c7c42eb58152f8ccd885c23e
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2022-08-31 13:04:32 +0530

    Some reports update

--- Assignments/Assign 3/Carry_Look_Ahead_Adder/CLA_16_bit_ripple.v
+++ Assignments/Assign 3/Carry_Look_Ahead_Adder/CLA_16_bit_ripple.v
@@ -19,7 +19,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module CLA_16_bit_ripple(input[15:0] in1, input[15:0] in2, input cin, output[15:0] out, output cout);
-wire[3:0] c;
+wire[2:0] c;
 
 CLA_4_bit a0(.in1(in1[3:0]),.in2(in2[3:0]),.cin(cin),.out(out[3:0]),.cout(c[0]));
 CLA_4_bit a1(.in1(in1[7:4]),.in2(in2[7:4]),.cin(c[0]),.out(out[7:4]),.cout(c[1]));
