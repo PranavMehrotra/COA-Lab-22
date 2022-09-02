e8889dd772fa1ba8c5c86ebd3fc784092c335e5c
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2022-09-02 16:39:43 +0530

    Some update in Q2

--- Assignments/Assign 4/Q2.s
+++ Assignments/Assign 4/Q2.s
@@ -124,13 +124,13 @@ swap:                                                                           
 
 # t2, 4($sp) : l
 # t3, 8($sp) : r
-# t4 : p
+# t4, 12($sp): p
 # a0,t5 : address of A
-# a1,t6 : left
-# a2,t7, 12($sp) : right
+# a1,t6, 16($sp) : left
+# a2,t7, 20($sp) : right
 
 sort_array:
-    addi $sp,$sp,-16
+    addi $sp,$sp,-24
     sw $ra,0($sp)
     move $t2,$a1
     move $t3,$a2
@@ -138,68 +138,79 @@ sort_array:
     move $t5,$a0
     move $t6,$a1
     move $t7,$a2
-    bge $t2,$t3,ret
-    while_l_p:
-        bge $t2,$t7,while_r_p
-        sll $s2,$t2,2
-        sll $s4,$t4,2
-        add $s2,$s2,$t5
-        add $s4,$s4,$t5
-        lw $s2,0($s2)
-        lw $s4,0($s4)
-        bgt $s2,$s4,while_r_p
-        addi $t2,$t2,1
-        j while_l_p
-    while_r_p:
-        ble $t3,$t6,if_l_r
-        sll $s3,$t3,2
-        sll $s4,$t4,2
-        add $s3,$s3,$t5
-        add $s4,$s4,$t5
-        lw $s3,0($s3)
-        lw $s4,0($s4)
-        blt $s3,$s4,if_l_r
-        addi $t3,$t3,-1
-        j while_r_p
-    if_l_r:
-        blt $t2,$t3,swap_l_r
-        sll $s4,$t4,2
-        sll $s3,$t3,2
-        add $a0,$t5,$s4
-        add $a1,$t5,$s3
-        jal swap
-        # Store l, r and right in stack, no other way.
-        sw $t2,4($sp)
-        sw $t3,8($sp)
-        sw $t7,12($sp)
-        move $a0,$t5
-        move $a1,$t6
-        move $a2,$t3
-        addi $a2,$a2,-1
-        jal sort_array
-        lw $t2,4($sp)
-        lw $t3,8($sp)
-        lw $t7,12($sp)
-        move $a0,$t5
-        move $a1,$t3
-        addi $a1,$a1,1
-        move $a2,$t7
-        sw $t2,4($sp)
-        sw $t3,8($sp)
-        jal sort_array
-        lw $t2,4($sp)
-        lw $t3,8($sp)
-    swap_l_r:
-        sll $s2,$t2,2
-        sll $s3,$t3,2
-        add $a0,$t5,$s2
-        add $a1,$t5,$s3
-        jal swap
-    j ret
+    while:
+        bge $t2,$t3,ret
+        while_l_p:
+            bge $t2,$t7,while_r_p
+            sll $s2,$t2,2
+            sll $s4,$t4,2
+            add $s2,$s2,$t5
+            add $s4,$s4,$t5
+            lw $s2,0($s2)
+            lw $s4,0($s4)
+            bgt $s2,$s4,while_r_p
+            addi $t2,$t2,1
+            j while_l_p
+        while_r_p:
+            ble $t3,$t6,if_l_r
+            sll $s3,$t3,2
+            sll $s4,$t4,2
+            add $s3,$s3,$t5
+            add $s4,$s4,$t5
+            lw $s3,0($s3)
+            lw $s4,0($s4)
+            blt $s3,$s4,if_l_r
+            addi $t3,$t3,-1
+            j while_r_p
+        if_l_r:
+            blt $t2,$t3,swap_l_r
+            sll $s4,$t4,2
+            sll $s3,$t3,2
+            add $a0,$t5,$s4
+            add $a1,$t5,$s3
+            jal swap
+            # Store l, r and right in stack, no other way.
+            sw $t2,4($sp)
+            sw $t3,8($sp)
+            sw $t4,12($sp)
+            sw $t6,16($sp)
+            sw $t7,20($sp)
+            move $a0,$t5
+            move $a1,$t6
+            move $a2,$t3
+            addi $a2,$a2,-1
+            jal sort_array
+            lw $t2,4($sp)
+            lw $t3,8($sp)
+            lw $t4,12($sp)
+            lw $t6,16($sp)
+            lw $t7,20($sp)
+            move $a0,$t5
+            move $a1,$t3
+            addi $a1,$a1,1
+            move $a2,$t7
+            sw $t2,4($sp)
+            sw $t3,8($sp)
+            sw $t4,12($sp)
+            sw $t6,16($sp)
+            sw $t7,20($sp)
+            jal sort_array
+            lw $t2,4($sp)
+            lw $t3,8($sp)
+            lw $t4,12($sp)
+            lw $t6,16($sp)
+            lw $t7,20($sp)
+        swap_l_r:
+            sll $s2,$t2,2
+            sll $s3,$t3,2
+            add $a0,$t5,$s2
+            add $a1,$t5,$s3
+            jal swap
+    j while
 
 ret:
     lw $ra,0($sp)
-    addi $sp,$sp,16
+    addi $sp,$sp,24
     j return
 
 
